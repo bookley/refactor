88f784e61f1929073cc996991f8b9680fd6d2e17
jamie <jamie.buckley@quantarc.co.uk>
2016-12-13 12:55:48 +0000

    moved asset urls to separate file

---
+++ client/engine/assetsUrls.ts
@@ -0,0 +1,63 @@
+
+export default [
+    {
+        url: "assets/shaders/texturedFrag.frag",
+        name: "texturedFrag",
+        type: "shader"
+    },
+    {
+        url: "assets/shaders/texturedVert.vert",
+        name: "texturedVert",
+        type: "shader"
+    },
+    {
+        url: "assets/shaders/instancedVert.vert",
+        name: "instancedVert",
+        type: "shader"
+    },
+    {
+        url: "assets/shaders/debugFrag.frag",
+        name: "debugFrag",
+        type: "shader"
+    },
+    {
+        url: "assets/shaders/debugVert.vert",
+        name: "debugVert",
+        type: "shader"
+    },
+    {
+        url: "assets/models/man.obj",
+        name: "man",
+        type: "mesh"
+    },
+    {
+        url: "assets/textures/mantex.png",
+        name: "mantexture",
+        type: "texture"
+    },
+    {
+        url: "assets/textures/wood.png",
+        name:"exclamation",
+        type:"texture"
+    },
+    {
+        url: "assets/textures/tileMap2.png",
+        name:"tileMap",
+        type:"texture"
+    },
+    {
+        url:"assets/textures/selectTile.png",
+        name:"selectTile",
+        type:"texture"
+    },
+    {
+        url:"assets/text/test.png",
+        name:"test",
+        type:"texture"
+    },
+    {
+        url:"assets/text/test.fnt",
+        name:"test_fnt",
+        type:"fnt"
+    },
+]
